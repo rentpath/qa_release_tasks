74597d105f3232e0b677a76774bf2ebaffbe6451
Jason Noble <perlwizard@gmail.com>
2009-03-31 11:42:26 -0400

    Fix release notes for first tag

--- lib/git/release_notes.rb
+++ lib/git/release_notes.rb
@@ -6,37 +6,23 @@ module Git
 
     def annotate!
       tags = get_tags.reverse
-
+      
       start_tag = ask "Start at which tag?", tags[0], tags
-      end_tag = ask "End at which tag?", tags[1] || tags[0], tags
-      end_index = tags.index(end_tag) + 1 # include end tag
-      start_index = tags.index(start_tag)
-      tags_to_log = tags[start_index..end_index]
-
-      puts      
-      if tags_to_log.size == 1
-        tag = tags_to_log.first
-        print_tag_header(tag)
-        puts tag_log(tag)
-      else
-        tags_to_log.each_cons(2) do |start, finish|
-          log = tag_log(finish, start)
-          next if log.empty?
-          print_tag_header(start)
-        end
-      end
+      end_tag = ask "End at which tag?", tags[1], tags
       puts
-    end
-    
-    private
-    
-    def print_tag_header(tag)
-      puts tag
-      puts "=" * tag.length
-    end
-    
-    def tag_log(finish, start=nil)
-      `git log --no-merges --pretty=format:"%h  %s" refs/tags/#{finish}#{'..refs/tags/' + start if start}`.strip
+      end_index = tags.index(end_tag) + 1 # include end tag
+      start_index = tags.index(start_tag)      
+      
+      start_index.upto(end_index-1) do |i|
+        start = tags[i]
+        finish = tags[i+1]
+        log = `git log --no-merges --pretty=format:"%h  %s" #{'refs/tags/' + finish + '..' if finish}refs/tags/#{start}`.strip
+        next if log.empty?
+        puts "#{start}"
+        puts "=" * start.length
+        puts log
+        puts
+      end
     end
   end
 end

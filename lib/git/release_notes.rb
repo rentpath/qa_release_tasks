406a03a509869cea5134c7a7a91088db0ab44ffa
Jason Noble <perlwizard@gmail.com>
2009-03-31 11:24:17 -0400

    Release notes handle specifying one tag

--- lib/git/release_notes.rb
+++ lib/git/release_notes.rb
@@ -8,19 +8,35 @@ module Git
       tags = get_tags.reverse
 
       start_tag = ask "Start at which tag?", tags[0], tags
-      end_tag = ask "End at which tag?", tags[1], tags
+      end_tag = ask "End at which tag?", tags[1] || tags[0], tags
       end_index = tags.index(end_tag) + 1 # include end tag
       start_index = tags.index(start_tag)
-      puts
+      tags_to_log = tags[start_index..end_index]
 
-      tags[start_index..end_index].each_cons(2) do |start, finish|
-        log = `git log --pretty=format:"%h  %s" refs/tags/#{finish}..refs/tags/#{start}`.strip
-        next if log.empty?
-        puts "#{start}"
-        puts "=" * start.length
-        puts log.split("\n").reject{|line| line.include?("Merge branch 'master' into qa_branch")}
-        puts
+      puts      
+      if tags_to_log.size == 1
+        tag = tags_to_log.first
+        print_tag_header(tag)
+        puts tag_log(tag)
+      else
+        tags_to_log.each_cons(2) do |start, finish|
+          log = tag_log(finish, start)
+          next if log.empty?
+          print_tag_header(start)
+        end
       end
+      puts
+    end
+    
+    private
+    
+    def print_tag_header(tag)
+      puts tag
+      puts "=" * tag.length
+    end
+    
+    def tag_log(finish, start=nil)
+      `git log --no-merges --pretty=format:"%h  %s" refs/tags/#{finish}#{'..refs/tags/' + start if start}`.strip
     end
   end
 end

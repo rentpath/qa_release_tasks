28158b9fb9adc163a8f7f6bfd70ac581e8326600
Jason Noble <perlwizard@gmail.com>
2010-06-21 16:07:18 -0400

    [JN/JS] [#000000] Remove merge functionality of tags

--- lib/git/tagger.rb
+++ lib/git/tagger.rb
@@ -11,14 +11,9 @@ module Git
     def tag!
       assert_is_git_repo
       begin
-        assert_on_qa_branch
-        system "git status"
-        update_qa if options[:update]
         fetch_tags
         tag_next_version(options)
         push_tags
-      ensure
-        system 'git checkout qa_branch' unless get_branch == 'qa_branch'
       end
     end
 
@@ -26,43 +21,6 @@ module Git
     
     def fetch_tags
       system("git fetch --tags")
-    end
-
-    def assert_on_qa_branch
-      unless get_branch == 'qa_branch'
-        error "You have to be in the qa_branch to do a release."
-      end
-    end
-
-    def update_qa
-      verify_update_is_ok
-      system("git checkout master")    &&
-      system("git pull --rebase")      &&
-      system("git checkout qa_branch") &&
-      system("git pull --rebase")      &&
-      response = %x(git merge master)
-
-      unless $?.success?
-        `git reset --hard`
-        error <<-EOS
-        Conflicts when updating the QA branch from master prevented a release from being created.
-        Please resolve these conflicts and then re-run rake qa:release:new.
-        EOS
-      else
-        system("git push")
-      end
-
-      unless response.include?("Fast forward") || response.include?('Already up-to-date.')
-        warn "There are outstanding changes in qa_branch that may need to be merged into master"
-      end
-    end
-    
-    def verify_update_is_ok
-      wrap do
-        answer = ask "This will pull the latest changes from master into the qa_branch. Continue?",
-            nil, /^yes|no$/i, "You must enter either 'yes' or 'no'"
-        abort "Exiting" unless answer.match(/^yes$/i)
-      end
     end
 
     def tag_next_version(options={})
